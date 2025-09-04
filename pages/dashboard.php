dec0dc3cda8758b14ce59633637bf1da114a6203
Anugrah Sandy Sulman Pratama <anugrahsandysulmanpratama@gmail.com>
2025-09-04 18:24:02 +0700

    REVISI 4

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -100,29 +100,7 @@ $subTitle = 'Aplikasi Manajamen Barang';
             </div>
         </div><!-- card end -->
     </div>
-    <div class="col">
-        <div class="card shadow-none border bg-gradient-start-5 h-100">
-            <div class="card-body p-20">
-                <div class="d-flex flex-wrap align-items-center justify-content-between gap-3">
-                    <?php
-                    $sql = "SELECT * FROM v_stok WHERE stok_tersedia < minimum_stock";
-                    $result = $link->query($sql);
-                    $count = $result->num_rows;
-                    ?>
-                    <div>
-                        <p class="fw-medium text-primary-light mb-1">Produk DIbawah Stok Minimal</p>
-                        <h6 class="mb-0"><?= $count ?></h6>
-                    </div>
-                    <div class="w-50-px h-50-px bg-red rounded-circle d-flex justify-content-center align-items-center">
-                        <iconify-icon icon="mingcute:battery-2-line" class="text-white text-2xl mb-0"></iconify-icon>
-                    </div>
-                </div>
-                <p class="fw-medium text-sm text-primary-light mt-12 mb-0 d-flex align-items-center gap-2">
-                    Total Produk Yang Sudah Dibawah Stok Minimal
-                </p>
-            </div>
-        </div><!-- card end -->
-    </div>
+    
     <div class="col-xxl-4 col-lg-6">
         <div class="card h-100">
 
@@ -159,39 +137,5 @@ $subTitle = 'Aplikasi Manajamen Barang';
             </div>
         </div>
     </div>
-    <div class="col-xxl-4 col-lg-6">
-        <div class="card h-100">
-
-            <div class="card-body">
-                <div class="d-flex align-items-center flex-wrap gap-2 justify-content-between mb-20">
-                    <h6 class="mb-2 fw-bold text-lg mb-0">Produk Dibawah Stok Minimal</h6>
-                    <a href="?page=persediaan-barang"
-                        class="text-primary-600 hover-text-primary d-flex align-items-center gap-1">
-                        Lihat Semua
-                        <iconify-icon icon="solar:alt-arrow-right-linear" class="icon"></iconify-icon>
-                    </a>
-                </div>
-
-                <div class="mt-32">
-                    <?php
-                    $sql = "SELECT * FROM v_stok WHERE stok_tersedia < minimum_stock";
-                    $result = $link->query($sql);
-                    while ($row = $result->fetch_assoc()) {
-                        ?>
-                        <div class="d-flex align-items-center justify-content-between gap-3 mb-32">
-                            <div class="d-flex align-items-center gap-2">
-                                <div class="flex-grow-1">
-                                    <h6 class="text-md mb-0 fw-normal"><?= $row['nama_produk'] ?></h6>
-                                    <span class="text-sm text-secondary-light fw-normal"><?php echo $row['kode_produk']; ?>
-                                        | Stok Minimal: <?= $row['minimum_stock'] ?></span>
-                                </div>
-                            </div>
-                            <span class="text-primary-light text-md fw-medium">Stok: <?= $row['stok_tersedia'] ?></span>
-                        </div>
-
-                    <?php } ?>
-                </div>
-            </div>
-        </div>
-    </div>
+    
 </div>
